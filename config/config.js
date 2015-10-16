b73539bf9d90246f124e33778fd4a0f8a140a567
Espen Hovlandsdal <rexxars@gmail.com>
2015-10-16 14:53:25 +0200

    Add configuration for AMQP, exchange and queue

--- config/config.js
+++ config/config.js
@@ -5,7 +5,21 @@ module.exports = {
         host: process.env.AMQP_HOST || 'localhost',
         port: process.env.AMQP_PORT || 5672,
         user: process.env.AMQP_USER || 'guest',
-        pass: process.env.AMQP_PASSWORD || 'guest',
+        password: process.env.AMQP_PASSWORD || 'guest',
         vhost: process.env.AMQP_VHOST || '/'
+    },
+
+    queue: {
+        name: '', // Leave blank to auto-generate
+        exclusive: true,
+        routingKey: ''
+    },
+
+    exchange: {
+        name: 'imbo'
+    },
+
+    consumption: {
+        noAck: true
     }
 };
